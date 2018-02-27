4b3cbfb1b868ce8a87a00297f6790b21f2d273fa
Adam Conder <adamdconder@gmail.com>
2018-02-27 15:48:23 +0000

    Add langing page

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/help', function (req, res) {
+  res.render('help')
+})
+
 // Add your routes here - above the module.exports line
 
 module.exports = router
